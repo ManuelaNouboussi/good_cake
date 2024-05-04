3737b364d11da4034b4867c739e4cd21449555be
manu7020@outlook.com <manu7020@outlook.com>
2024-05-04 19:21:19 -0400

    starting...

---
+++ server.js
@@ -0,0 +1,15 @@
+// Initialize server and set up listener
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const dispositionsRedBall = [
+  { id: 'maisonDeLaPatisserie', label: 'Maison de la Patisserie' },
+  { id: 'mesSecretsSucres', label: 'Mes Secrets Sucrés' },
+  { id: 'mesSecretsSucresPartages', label: 'Mes Secrets Sucrés Partagés' },
+  { id: 'tribunalDesAdjuges', label: 'Tribunal des Adjugés' },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <h1>Ma Barre de Navigation</h1>
+      </nav>
+      <div className="diagonal-container">
+        {dispositionsRedBall.map(({ id, label }) => (
+          <div key={id} id={id} className="red-ball">
+            <h2>{label}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
